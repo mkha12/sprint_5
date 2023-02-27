6b106431e7b63376d76e5ea6a3c5db6be7f2fdca
mkha12 <123381512+mkha12@users.noreply.github.com>
2023-02-27 14:16:38 +0400

    try1

---
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -0,0 +1,138 @@
+import UIKit
+
+final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
+    
+    @IBOutlet private var questionTitleLabel: UILabel!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var counterLabel: UILabel!
+    @IBOutlet private var noButton: UIButton!
+    @IBOutlet private var yesButton: UIButton!
+    private var currentQuestionIndex: Int = 0
+    private var correctAnswers: Int = 0
+    private let questionsAmount: Int = 10
+    private var questionFactory: QuestionFactoryProtocol?
+    private var currentQuestion: QuizQuestion?
+    private var statisticService: StatisticService?
+    @IBAction private func noButtonClicked(_ sender: Any) {
+        guard let currentQuestion = currentQuestion else {
+            return
+        }
+        let givenAnswer = false
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+    }
+    
+    @IBAction private func yesButtonClicked(_ sender: Any) {
+        guard let currentQuestion = currentQuestion else {
+            return
+        }
+        let givenAnswer = true
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+    }
+   
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        questionFactory = QuestionFactory(delegate: self)
+        questionFactory?.requestNextQuestion(by: currentQuestionIndex)
+        noButton.titleLabel?.font =  UIFont(name: "YSDisplay-Medium", size: 20)
+        yesButton.titleLabel?.font =  UIFont(name: "YSDisplay-Medium", size: 20)
+        textLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
+        counterLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        questionTitleLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        statisticService = StatisticServiceImplementation()
+        
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
+        
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        
+        textLabel.text = step.question
+        imageView.image = step.image
+        counterLabel.text = step.questionNumber
+        imageView.layer.borderColor = UIColor.clear.cgColor
+        
+    }
+
+    private func show(quiz result: QuizResultsViewModel) {
+        let alertViewModel = AlertModel(title: result.title, message: result.text, buttonText: result.buttonText, completion: { [weak self] _ in
+            guard let self = self else { return }
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            self.questionFactory?.requestNextQuestion(by: self.currentQuestionIndex)
+        }
+        )
+        
+        let alert = AlertPresenter()
+        alert.present(view: self, alert: alertViewModel)
+    }
+    
+    func didReceiveNextQuestion(question: QuizQuestion?) {
+        guard let question = question else {
+            return
+        }
+        
+        currentQuestion = question
+        let viewModel = convert(model: question)
+        DispatchQueue.main.async { [weak self] in
+            self?.show(quiz: viewModel)
+        }
+    }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        
+        if isCorrect {
+            correctAnswers += 1
+        }
+        
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
+            self.showNextQuestionOrResults()
+        }
+        
+    }
+    
+    private func showNextQuestionOrResults() {
+        guard let questionFactory = questionFactory else {
+            return
+        }
+        
+        if currentQuestionIndex == questionsAmount - 1 {
+            guard let statisticService = statisticService else {
+                return
+            }
+            statisticService.store(correct: correctAnswers, total: questionsAmount)
+            
+            
+            let text = correctAnswers == questionsAmount ?
+            "Поздравляем, Вы ответили на 10 из 10!\n" :
+            "Ваш результат \(correctAnswers) из 10\n" +
+            "Количество сыгранных квизов \(statisticService.gamesCount)\n" +
+            "Рекорд \(statisticService.bestGame.correct)/\(statisticService.bestGame.total) (\(statisticService.bestGame.date.dateTimeString))\n" +
+            "Средняя точность: \(String(format: "%.2f", statisticService.totalAccuracy))%"
+            
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть ещё раз")
+            show(quiz: viewModel)
+        } else {
+            imageView.layer.borderColor = UIColor.clear.cgColor
+            currentQuestionIndex += 1
+            questionFactory.requestNextQuestion(by: currentQuestionIndex)
+        }
+    }
+}
